cfd6039ef794d4145b2efaa523f7ac199bbb0ad8
ian.chelaru <ianchelaru@yahoo.com>
2019-12-09 19:51:28 +0200

    first commit

---
+++ src/main/java/com/ianchelaru/sport_events_server/SportEventsServerApplication.java
@@ -0,0 +1,13 @@
+package com.ianchelaru.sport_events_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEventsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportEventsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ianchelaru/sport_events_server/SportEventsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianchelaru.sport_events_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportEventsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
